e509cd062af94c09d02427b89048392de98c1fc2
forgeronduweb <forgeronduweb@gmail.com>
2025-09-05 04:42:00 +0000

    Delete backend directory

--- backend/src/auth/decorators/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-// UserRole sera disponible après génération du client Prisma
-
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: ('FREELANCE' | 'CLIENT' | 'ADMIN')[]) => SetMetadata(ROLES_KEY, roles);
--- backend/src/auth/dto/forgot-password.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class ForgotPasswordDto {
-  @IsEmail({}, { message: 'Email invalide' })
-  @IsNotEmpty({ message: 'Email requis' })
-  email: string;
-}
