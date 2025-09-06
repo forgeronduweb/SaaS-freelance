f32cbecea9d99f3bca67751596ea2d7d5e3786aa
forgeronduweb <forgeronduweb@gmail.com>
2025-09-06 11:06:11 +0000

    Delete src directory

--- src/app/clients/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import ClientProfile from '@/components/client/ClientProfile';
-import { DynamicPageProps } from '@/types/page';
-
-export default async function ClientProfilePage({ params }: DynamicPageProps) {
-  const { id } = await params;
-  return <ClientProfile clientId={id} />;
-}
--- src/app/missions/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import MissionDetail from '@/components/mission/MissionDetail';
-import { DynamicPageProps } from '@/types/page';
-
-export default async function MissionDetailPage({ params }: DynamicPageProps) {
-  const { id } = await params;
-  return <MissionDetail missionId={id} />;
-}
