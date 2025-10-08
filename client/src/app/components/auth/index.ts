50d9b3ffe01c0523967eee7f306f3ee0ee6e6472
forgeronduweb <forgeronduweb@gmail.com>
2025-10-08 07:53:33 +0000

    Delete client/src/app directory

--- client/src/app/components/auth/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Login } from './Login';
-export { default as ClientSignup } from './ClientSignup';
-export { default as FreelanceSignup } from './FreelanceSignup';
-export { default as ResetPassword } from './ResetPassword';
